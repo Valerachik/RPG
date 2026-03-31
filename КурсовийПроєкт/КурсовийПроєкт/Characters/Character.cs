e7d7964df1f8520578ff74d2993f8e1a00317da2
unknown <ditrikh.valeriia@lll.kpi.ua>
2026-03-31 19:50:22 +0300

    added few base classes

---
+++ КурсовийПроєкт/КурсовийПроєкт/Characters/Character.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КурсовийПроєкт.Characters
+{
+    internal class Character
+    {
+    
+    }
+
+    public class MainCharacter
+    {
+
+    }
+    public class Enemy
+    {
+
+    }
+}
